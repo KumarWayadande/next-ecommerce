308bd6d4736c9396a64dd5a1835dad8cb6e960d5
Kumar Wayadande <kumarwaidande7219@gmail.com>
2025-02-01 14:46:00 +0530

    modified:   src/app/[slug]/page.tsx 	new file:   src/components/Footer.tsx 	new file:   src/components/Navbar.tsx

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,1 +1,14 @@
+"use client"
+import { usePathname } from 'next/navigation'
+    
+    function SinglePage() {
+        const pathName = usePathname();
+      return (
+        <div>
+          {pathName}
+        </div>
+      )
+    }
+    
+    export default SinglePage
     
