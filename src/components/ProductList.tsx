fedd102a8889f1b52f4c504df5946762ced09566
Kumar Wayadande <kumarwaidande7219@gmail.com>
2025-02-03 21:06:50 +0530

    new file:   src/components/ProductList.tsx

---
+++ src/components/ProductList.tsx
@@ -0,0 +1,141 @@
+import Image from "next/image";
+import Link from "next/link";
+
+function ProductList() {
+  return (
+    <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap ">
+      <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+        <div className="wrapper relative w-full h-80">
+          <Image
+            src="https://images.pexels.com/photos/30374245/pexels-photo-30374245/free-photo-of-solitary-figure-walking-in-vast-desert-landscape.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            sizes="25vw"
+            alt="Cart Product Image"
+            fill
+            className="absolute rounded-md object-cover z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/27240875/pexels-photo-27240875/free-photo-of-a-house-with-a-red-roof-and-trees-in-front.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            sizes="25vw"
+            alt="Cart Product Image"
+            fill
+            className="absolute rounded-md object-cover"
+          />
+        </div>
+        <div className="flex justify-between">
+            <span className="font-medium">Product Name</span>
+            <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">
+            My Description
+        </div>
+        <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4  text-xs hover:bg-lama hover:text-white ">Add to Cart</button>
+      </Link>
+      <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+        <div className="wrapper relative w-full h-80">
+          <Image
+            src="https://images.pexels.com/photos/30374245/pexels-photo-30374245/free-photo-of-solitary-figure-walking-in-vast-desert-landscape.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            sizes="25vw"
+            alt="Cart Product Image"
+            fill
+            className="absolute rounded-md object-cover z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/27240875/pexels-photo-27240875/free-photo-of-a-house-with-a-red-roof-and-trees-in-front.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            sizes="25vw"
+            alt="Cart Product Image"
+            fill
+            className="absolute rounded-md object-cover"
+          />
+        </div>
+        <div className="flex justify-between">
+            <span className="font-medium">Product Name</span>
+            <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">
+            My Description
+        </div>
+        <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4  text-xs hover:bg-lama hover:text-white ">Add to Cart</button>
+      </Link>
+      <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+        <div className="wrapper relative w-full h-80">
+          <Image
+            src="https://images.pexels.com/photos/30374245/pexels-photo-30374245/free-photo-of-solitary-figure-walking-in-vast-desert-landscape.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            sizes="25vw"
+            alt="Cart Product Image"
+            fill
+            className="absolute rounded-md object-cover z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/27240875/pexels-photo-27240875/free-photo-of-a-house-with-a-red-roof-and-trees-in-front.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            sizes="25vw"
+            alt="Cart Product Image"
+            fill
+            className="absolute rounded-md object-cover"
+          />
+        </div>
+        <div className="flex justify-between">
+            <span className="font-medium">Product Name</span>
+            <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">
+            My Description
+        </div>
+        <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4  text-xs hover:bg-lama hover:text-white ">Add to Cart</button>
+      </Link>
+      <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+        <div className="wrapper relative w-full h-80">
+          <Image
+            src="https://images.pexels.com/photos/30374245/pexels-photo-30374245/free-photo-of-solitary-figure-walking-in-vast-desert-landscape.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            sizes="25vw"
+            alt="Cart Product Image"
+            fill
+            className="absolute rounded-md object-cover z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/27240875/pexels-photo-27240875/free-photo-of-a-house-with-a-red-roof-and-trees-in-front.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            sizes="25vw"
+            alt="Cart Product Image"
+            fill
+            className="absolute rounded-md object-cover"
+          />
+        </div>
+        <div className="flex justify-between">
+            <span className="font-medium">Product Name</span>
+            <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">
+            My Description
+        </div>
+        <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4  text-xs hover:bg-lama hover:text-white ">Add to Cart</button>
+      </Link>
+      <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+        <div className="wrapper relative w-full h-80">
+          <Image
+            src="https://images.pexels.com/photos/30374245/pexels-photo-30374245/free-photo-of-solitary-figure-walking-in-vast-desert-landscape.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            sizes="25vw"
+            alt="Cart Product Image"
+            fill
+            className="absolute rounded-md object-cover z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/27240875/pexels-photo-27240875/free-photo-of-a-house-with-a-red-roof-and-trees-in-front.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            sizes="25vw"
+            alt="Cart Product Image"
+            fill
+            className="absolute rounded-md object-cover"
+          />
+        </div>
+        <div className="flex justify-between">
+            <span className="font-medium">Product Name</span>
+            <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">
+            My Description
+        </div>
+        <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4  text-xs hover:bg-lama hover:text-white ">Add to Cart</button>
+      </Link>
+    </div>
+  );
+}
+
+export default ProductList;
