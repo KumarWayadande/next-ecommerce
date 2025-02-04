ff249eb004c5bc7e7680dff834a23b3a297882a3
Kumar Wayadande <kumarwaidande7219@gmail.com>
2025-02-04 13:06:42 +0530

    modified:   src/components/ProductImages.tsx

--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -1,17 +1,75 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
+
+const images = [
+  {
+    id: 1,
+    url: "https://images.pexels.com/photos/19036832/pexels-photo-19036832/free-photo-of-mountain-reflection-in-lake.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+  },
+  {
+    id: 2,
+    url: "https://images.pexels.com/photos/17867705/pexels-photo-17867705/free-photo-of-crowd-of-hikers-on-the-mountain-ridge-at-dusk.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+  },
+  {
+    id: 3,
+    url: "https://images.pexels.com/photos/21812160/pexels-photo-21812160/free-photo-of-puerta-colonial-color-rojo-de-guanajuato-mexico.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+  },
+  {
+    id: 4,
+    url: "https://images.pexels.com/photos/20832069/pexels-photo-20832069/free-photo-of-a-narrow-street-with-buildings-and-cars.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+  },
+//   {
+//     id: 5,
+//     url: "https://images.pexels.com/photos/20832069/pexels-photo-20832069/free-photo-of-a-narrow-street-with-buildings-and-cars.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+//   },
+//   {
+//     id: 6,
+//     url: "https://images.pexels.com/photos/20832069/pexels-photo-20832069/free-photo-of-a-narrow-street-with-buildings-and-cars.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+//   },
+//   {
+//     id: 7,
+//     url: "https://images.pexels.com/photos/20832069/pexels-photo-20832069/free-photo-of-a-narrow-street-with-buildings-and-cars.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+//   },
+//   {
+//     id: 8,
+//     url: "https://images.pexels.com/photos/20832069/pexels-photo-20832069/free-photo-of-a-narrow-street-with-buildings-and-cars.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+//   },
+];
 
 function ProductImages() {
+  const [index, setIndex] = useState(0);
+
   return (
     <div>
       <div className="h-[500px] relative">
         <Image
-          src="https://images.pexels.com/photos/30374245/pexels-photo-30374245/free-photo-of-solitary-figure-walking-in-vast-desert-landscape.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          src={images[index].url}
           fill
           sizes="50vw"
           alt=""
+          className="object-cover rounded-md"
         />
       </div>
-      <div></div>
+      <div className="flex flex-row justify-between gap-4 mt-8">
+        {images.map((image, i) => {
+          return (
+            <div
+              className="w-1/4 cursor-pointer h-32 relative gap-4 mt-8"
+              key={image.id}
+              onClick={() => setIndex(i)}
+            >
+              <Image
+                src={image.url}
+                fill
+                sizes="30vw"
+                alt=""
+                className="object-cover rounded-md"
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
